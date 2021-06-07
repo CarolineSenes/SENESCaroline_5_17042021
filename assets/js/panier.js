e6503cd6f0cae752beee334f040522cadea22bd1
SENES <caro67200@gmail.com>
2021-06-07 23:51:11 +0200

    test heroku avec confirmation.html
    test précédent avec produit.html OK

--- assets/js/panier.js
+++ assets/js/panier.js
@@ -233,7 +233,7 @@ document
             console.log('Données à envoyer : ', aEnvoyer);
         
             //on envoie nos données au serveur
-            fetch(`https://orinoco-app-ecommerce.herokuapp.com/api/cameras/order`, {
+            fetch(`${apiUrl}/api/cameras/order`, {
                 method: "POST",
                 headers: {
                 "Content-Type": "application/json"
